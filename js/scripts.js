20e6a8d6442f2287c0aa3d043f2b259d0154cc46
milkyWRLD <heavydisto@gmail.com>
2025-10-14 22:35:30 +0300

    Выгрузка проекта

---
+++ js/scripts.js
@@ -0,0 +1,146 @@
+﻿var speedPopupShow = 150;
+
+function showPopupPresent() {
+	$(".overlay, .popup_present").fadeTo(speedPopupShow, 1);
+}
+function showPopupSuccess() {
+	$('.popup_success, .overlay').fadeTo(speedPopupShow, 1);
+}
+function showPopupVideo(srcres) {
+	$("#video-content iframe").attr("src", srcres);
+	$('.popup_video, .overlay').fadeTo(speedPopupShow, 1);
+}
+function closeAllPopup() {
+	$('.overlay, .popup_present, .popup_video, .popup_success').fadeTo(speedPopupShow, 0, function() {
+		$("#video-content iframe").attr("src", "");	
+		setTimeout(function () {
+			$('.overlay, .popup_present, .popup_video, .popup_success').hide();
+		}, 500);		
+	});
+}
+
+$(function() {
+	$(document).ready(function(){
+		$("#scrollTop").click(function(e) {
+			e.preventDefault();
+			$('html, body').animate({ scrollTop: 0 }, 'slow');
+		});
+
+		$(".btn, .btn-img, .btn-call").click(function(e){
+			// Исключение для кнопок слайдера отзывов и калькулятора
+			if ($(this).is('#prev-review, #next-review, #calc-form button')) return;
+			e.preventDefault();
+			showPopupPresent();
+		});
+
+		$("#closePopupSuccess").click(function(e){
+			e.preventDefault();
+			closeAllPopup();
+		});
+
+		$(".video-link").click(function(e){
+			var res = $(this).attr("data-res");
+			e.preventDefault();
+			showPopupVideo(res);
+		});
+
+		$('.close_popup, .overlay').click(function (){        
+			closeAllPopup();
+		});
+
+		$("#submitPresentForm").click(function(e){
+			e.preventDefault();
+			$("#popup_present_form").submit();
+			$('.popup_present').fadeTo(speedPopupShow, 0, function() {
+				setTimeout(function () {
+					$('popup_present').hide();
+					showPopupSuccess();
+				}, 500);        
+			});
+		});
+
+		$("#popup_present_form").ajaxForm({
+			success: function(responseText) {
+				// Добавляем цель в яндекс.метрику
+				yaCounter38407175.reachGoal('SubmitPresentForm');
+			},
+			error: function(res, val) {
+				console.log(res);
+				console.log(val);
+			}
+		});
+
+		// Карусель отзывов и кейсов
+		let reviewIndex = 0;
+		const $carousel = $(".reviews-carousel");
+		const $reviews = $carousel.find(".review");
+		function showReview(idx) {
+			$reviews.removeClass("active");
+			$reviews.eq(idx).addClass("active");
+		}
+		showReview(reviewIndex);
+		$("#prev-review").click(function(){
+			reviewIndex = (reviewIndex - 1 + $reviews.length) % $reviews.length;
+			showReview(reviewIndex);
+		});
+		$("#next-review").click(function(){
+			reviewIndex = (reviewIndex + 1) % $reviews.length;
+			showReview(reviewIndex);
+		});
+		// Swipe для мобильных
+		let startX = null;
+		$carousel.on('touchstart', function(e) {
+			startX = e.originalEvent.touches[0].clientX;
+		});
+		$carousel.on('touchend', function(e) {
+			if (startX === null) return;
+			let endX = e.originalEvent.changedTouches[0].clientX;
+			if (endX - startX > 40) { // swipe right
+				reviewIndex = (reviewIndex - 1 + $reviews.length) % $reviews.length;
+				showReview(reviewIndex);
+			} else if (startX - endX > 40) { // swipe left
+				reviewIndex = (reviewIndex + 1) % $reviews.length;
+				showReview(reviewIndex);
+			}
+			startX = null;
+		});
+
+		// Калькулятор стоимости (современная логика)
+		// Расчет стоимости по типу услуги, площади и дополнительным опциям
+		$("#calc-form").on("submit", function(e){
+			e.preventDefault();
+			// Получаем значения формы
+			const service = $(this).find('[name="service"]').val();
+			const area = parseInt($(this).find('[name="area"]').val(), 10);
+			const option = $(this).find('[name="options"]').val();
+			let base = 0, perM2 = 0;
+			// Базовые цены и цена за м2
+			switch(service) {
+				case 'well': base = 80000; perM2 = 100; break;
+				case 'heating': base = 50000; perM2 = 350; break;
+				case 'sewer': base = 40000; perM2 = 200; break;
+				case 'water': base = 30000; perM2 = 120; break;
+			}
+			let price = base + area * perM2;
+			// Обработка дополнительных опций
+			if(option === 'fast') price += 10000;
+			if(option === 'warranty') price += 7000;
+			// Плавный вывод результата
+			$("#calc-result").hide().text('Ориентировочная стоимость: ' + price.toLocaleString('ru-RU') + ' ₽').fadeIn(200);
+		});
+		// Логика: базовая стоимость + площадь * ставка + опции. Все значения легко менять в switch/if.
+
+		// Плавный скролл для всех якорных ссылок
+		if ('scrollBehavior' in document.documentElement.style) {
+		  document.documentElement.style.scrollBehavior = 'smooth';
+		} else {
+		  $(document).on('click', 'a[href^="#"]', function(e) {
+		    var target = $(this.getAttribute('href'));
+		    if (target.length) {
+		      e.preventDefault();
+		      $('html, body').animate({ scrollTop: target.offset().top }, 600);
+		    }
+		  });
+		}
+	});    
+});
---
+++ mail.php
@@ -0,0 +1,58 @@
+﻿<?php
+
+$name=$_POST["name"];
+$phone=$_POST["phone"];
+
+if (isset ($name))
+{
+$name = substr($name,0,35); //Не может быть более 35 символов
+if (empty($name))
+{
+echo "<center><b>Не указано имя !!!<p>";
+echo "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
+exit;
+}
+}
+else
+{
+$name = "не указано";
+}
+if (isset ($phone))
+{
+$phone = substr($phone,0,20); //Не может быть более 20 символов
+if (empty($phone))
+{
+echo "<center><b>Не указан телефон !!!<p>";
+echo "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
+exit;
+}
+}
+else
+{
+$phone = "не указано";
+}
+
+$i = "не указано";
+if ($name == $i AND $phone == $i)
+{
+echo "Ошибка ! Скрипту не были переданы параметры !";
+exit;
+}
+
+function sendMail($fname, $fphone) {
+	$to = "vash-montag24@mail.ru";
+	$subject = "Заявка с сайта РадаСтрой\n";
+	$message = "Поступила заявка с сайта РадаСтрой\n";
+	$message .= "Имя: $fname\n";
+	$message .= "Телефон: $fphone\n";
+	$message .= "Письмо сгенерированно автоматический\n";
+	$header = "Content-type: text/plain; charset=\"utf-8\"";
+
+	mail ($to,$subject,$message,$header) or print "Не могу отправить письмо !!!";
+}
+
+sendMail($name, $phone); //Отправляем на почту
+
+exit;
+
+?>
